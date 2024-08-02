981c552120fc2aefe541a1bce910c0060c91faa9
jaiyankargupta <jaiyankargupta.com>
2024-08-02 15:16:44 +0530

    added testCases folder, read_csv file and plot_curve file

---
+++ plot_curves.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_csv(csv_path):
+    np_path_XYs = np.genfromtxt(csv_path, delimiter=',')
+    path_XYs = []
+    for i in np.unique(np_path_XYs[:, 0]):
+        npXYs = np_path_XYs[np_path_XYs[:, 0] == i][:, 1:]
+        XYs = []
+        for j in np.unique(npXYs[:, 0]):
+            XY = npXYs[npXYs[:, 0] == j][:, 1:]
+            XYs.append(XY)
+        path_XYs.append(XYs)
+    return path_XYs
+
+def plot(paths_XYs):
+    fig, ax = plt.subplots(tight_layout=True, figsize=(8, 8))
+    colours = plt.cm.viridis(np.linspace(0, 1, len(paths_XYs)))
+    for i, XYs in enumerate(paths_XYs):
+        c = colours[i % len(colours)]
+        for XY in XYs:
+            ax.plot(XY[:, 0], XY[:, 1], c=c, linewidth=2)
+    ax.set_aspect('equal')
+    plt.show()
+
+# Example usage
+csv_folder = "testCases"
+csv_files = ["isolated.csv", "frag0.csv", "frag1.csv", "occlusion1.csv"]
+
+for csv_file in csv_files:
+    csv_path = os.path.join(csv_folder, csv_file)
+    paths_XYs = read_csv(csv_path)
+    plot(paths_XYs)
